f96af4174a913d0a70348586f0119ccd7a3b90ee
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2023-06-03 12:45:26 +0530

    updated nvchad

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,9 +1,14 @@
 -- First read our docs (completely) then check the example_config repo
 
+---@type ChadrcConfig
 local M = {}
 
+-- Path to overriding theme and highlights files
+-- local highlights = require "custom.highlights"
+
 M.ui = {
-  theme = "ayu-dark",
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
 }
 
 M.plugins = require "custom.plugins"
