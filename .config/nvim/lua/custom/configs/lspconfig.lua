5623dc159f3802b6e1ad83fc3c258d267d450a11
Upendra Upadhyay <upendra.upadhyay@gojek.com>
2023-09-02 13:21:37 +0530

    using ayu dark

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "pyright", "hls", "nil_ls"}
+local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "pyright", "hls", "nil_ls", "clojure_lsp"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
