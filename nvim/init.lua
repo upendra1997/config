290464cc9e5dab2a5e10c9463a8c832e339df081
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-02-11 10:45:28 +0530

    enabling system clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set expandtab")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set nu")
+vim.cmd("set clipboard+=unnamedplus")
 
 require("lazy").setup({
   "joom/vim-commentary"
