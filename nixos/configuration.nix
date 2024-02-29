78715bcfd6e4c252499620314d134ad944ee12c4
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-02-29 14:31:49 +0530

    disabling log-level for network emulators

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,8 +16,10 @@
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable =
-    true; # Easiest to use and most distros use this by default.
+  networking.networkmanager = {
+    enable = true; # Easiest to use and most distros use this by default.
+    # logLevel = "DEBUG";
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
