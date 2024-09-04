fd39cb1e989f5d57e580950c1bb5b10e7c9e2da9
Upendra Upadhyay <upendra.upadhyay@gojek.com>
2024-09-04 16:52:08 +0530

    adding wezterm changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -37,18 +37,18 @@ end
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font 'FiraCode Nerd Font'
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.80
 config.window_decorations = "RESIZE"
 -- config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
 config.font_size = 16.0
 
-local schemes = schemes()
+-- local schemes = schemes()
 
-local theme = schemes["gogh"]
-local random_theme = theme[math.random(#theme)]
-wezterm.log_error(random_theme)
+-- local theme = schemes["gogh"]
+-- local random_theme = theme[math.random(#theme)]
+-- wezterm.log_error(random_theme)
 -- config.color_scheme = "GruvboxDark" -- random_theme
 config.color_scheme = 'Everforest Dark Hard (Gogh)'
 
