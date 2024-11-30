8b42bc5c6b991d126bffe1e71128deb65c49900f
Upendra Upadhyay <upendra@stripe.com>
2024-11-30 23:23:08 +0530

    Migrating to Mac M3
    Removing unsupported brew on stripe

--- nix-darwin/darwin-configuration.nix
+++ nix-darwin/darwin-configuration.nix
@@ -12,7 +12,9 @@ in {
     name = username;
     home = "/Users/${username}";
   };
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
+  home-manager.backupFileExtension = "bkp";
   home-manager.users.upendra = { pkgs, ... }: {
     nixpkgs.config.allowUnfree = true;
     home = {
@@ -66,7 +68,7 @@ in {
           xcodebuild
           slack
           redis
-          teleport_14
+          teleport
           obsidian
           k9s
           cloak
@@ -79,18 +81,17 @@ in {
           rectangle
           colima
           docker
-          gnupg
+          # gnupg
           openssh
-          nerdfonts
           pyright
           totp
           nixfmt-classic
           openfortivpn
           zoom-us
           aegis
-          drawio
+          # drawio
           colima
-          ffmpeg_5-full
+          ffmpeg-full
           imagemagick
           delta
           bat
@@ -104,19 +105,19 @@ in {
           mupdf
           ghostscript
           lua-language-server
-          transmission
+          transmission_4
           nom
           ghc
           haskell-language-server
           flameshot
           stern
-          customClojure
+          # customClojure
           m-cli
-          losslesscut-bin
           zathura
-          # lorien
-          # rnote
-        ];
+          maccy
+          # stats
+          # discord
+        ] ++ builtins.filter lib.isDerivation (builtins.attrValues nerd-fonts);
       shellAliases = { vim = "nvim"; };
       sessionVariables = {
         EDITOR = "nvim";
@@ -127,45 +128,45 @@ in {
       enable = true;
       userName = "Upendra Upadhyay";
       userEmail = "upendra.upadhyay.97@gmail.com";
-      signing = {
-        key = "A8653EBC1131DDCF";
-        signByDefault = true;
-      };
+      # signing = {
+      #  key = "A8653EBC1131DDCF";
+      #  signByDefault = true;
+      # };
       # difftastic.enable = true;
       delta.enable = true;
     };
     programs.direnv.enable = true;
     programs.fzf.enable = true;
     programs.direnv.nix-direnv.enable = true;
-    programs.zsh = {
-      enable = true;
-      syntaxHighlighting.enable = true;
-      autosuggestion.enable = true;
-      enableVteIntegration = true;
-      history.expireDuplicatesFirst = true;
-      history.extended = true;
-      history.size = 99999999999999999;
-      initExtraFirst = ''
-        export PATH="/Users/upendra/bin:$PATH"
-      '';
-      profileExtra = ''
-        export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
-      '';
-      prezto.enable = true;
-      antidote.enable = true;
-      autocd = true;
-      oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-        plugins = [ "git" "direnv" "fzf" "gpg-agent" ];
-      };
-    };
+    # programs.zsh = {
+    #   enable = true;
+    #   syntaxHighlighting.enable = true;
+    #   autosuggestion.enable = true;
+    #   enableVteIntegration = true;
+    #   history.expireDuplicatesFirst = true;
+    #   history.extended = true;
+    #   history.size = 99999999999999999;
+    #   initExtraFirst = ''
+    #     export PATH="/Users/upendra/bin:$PATH"
+    #   '';
+    #   profileExtra = ''
+    #     export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+    #   '';
+    #   prezto.enable = true;
+    #   antidote.enable = true;
+    #   autocd = true;
+    #   oh-my-zsh = {
+    #     enable = true;
+    #     theme = "robbyrussell";
+    #     plugins = [ "git" "direnv" "fzf" "gpg-agent" ];
+    #   };
+    # };
     programs.neovim = {
       enable = true;
       plugins = with pkgs.vimPlugins; [ ];
     };
 
-    programs.gpg.enable = true;
+    # programs.gpg.enable = true;
     # programs.gpg.publicKeys = [ { source = ./pubkeys.txt; } ];
 
     home.stateVersion = "24.11";
@@ -187,41 +188,42 @@ in {
 
   homebrew = {
     enable = true;
-    brews = [ "postgresql"
-              "redis"
-              "consul"
-              "mongodb-community" ];
-    taps = [ "mongodb/brew" "grishka/grishka" ];
+    brews = [
+             # "postgresql"
+             # "redis"
+             # "consul"
+             # "mongodb-community"
+           ];
+    taps = [
+            # "mongodb/brew"
+            # "grishka/grishka"
+           ];
     casks = [
-      "firefox"
-      "whatsapp"
-      "postman"
-      "jetbrains-toolbox"
-      "discord"
-      "wacom-tablet"
-      "openboard"
-      "stremio"
-      "tomighty"
-      "microsoft-remote-desktop"
-      "maccy"
-      "obs"
-      "the-unarchiver"
-      "stats"
-      "flameshot"
-      "trilium-notes"
-      "grishka/grishka/neardrop"
-      "deskpad"
-      # "xournal-plus-plus"
-      # "krita"
+       "obs"
+       "jetbrains-toolbox"
+       # "whatsapp"
+       # "firefox"
+       # "postman"
+       # "wacom-tablet"
+       # "openboard"
+       # "stremio"
+       # "tomighty"
+       # "microsoft-remote-desktop"
+       # "the-unarchiver"
+       # "trilium-notes"
+       # "grishka/grishka/neardrop"
+       # "deskpad"
+       # "xournal-plus-plus"
+       # "krita"
     ];
   };
 
   programs.zsh.enable = true;
 
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # programs.fish.enable = true;
 
---
+++ nix-darwin/rebuild-switch.sh
@@ -0,0 +1,1 @@
+darwin-rebuild switch --impure --show-trace --flake /Volumes/personal/config/nix-darwin
