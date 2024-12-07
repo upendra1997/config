e4a3c72bfaff1c641ffd02cc3e9c9ce0e27865de
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-12-07 21:55:17 +0530

    upgrading 24.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,12 +1,17 @@
 {
-  inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
+  # inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
 
-  outputs = { self, nixpkgs, vscode-server }: {
+  outputs = { self, nixpkgs }: # , vscode-server }:
+      let
+      system = builtins.currentSystem;
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
-        vscode-server.nixosModules.default
-        ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
+        # vscode-server.nixosModules.default
+        # ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
       ];
     };
   };
