b199a789a4c29a5f2e2caca97695cd862751cb0f
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2024-12-24 22:01:39 +0530

    adding slack-term

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -125,6 +125,7 @@ in
           gdb
           delve
           dig
+          slack-term
         ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPweedtJiRm/qMcdmudRT7aW4xCi7vT0nmBEhv7gDWmq" # android
@@ -154,7 +155,9 @@ in
           mypaint
           lutris
           zathura
+          # vscode
           # eww
+          # nerdfonts
         ];
       };
 
@@ -173,6 +176,7 @@ in
         docker
         coreutils-full
         pkg-config
+        slack-term
       ];
 
       # Some programs need SUID wrappers, can be configured further or are
