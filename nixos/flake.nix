a53476f0d9712160241aeb33e0b6f7bce9f686fc
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2025-05-31 00:37:08 +0530

    formatting fix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,22 +1,19 @@
 {
   inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
 
   outputs = { self, nixpkgs, vscode-server }:
-      let
+    let
       # system = "x86_64-linux"; # check if we can remove --impure from the configuration.nix
       system = builtins.currentSystem;
       pkgs = import nixpkgs { inherit system; };
-    in
-    {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-        vscode-server.nixosModules.default
-        ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
-      ];
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          vscode-server.nixosModules.default
+          ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
+        ];
+      };
     };
-  };
 }
