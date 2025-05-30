bb134d999ef24b3352a70dacf3069dfcf32686c9
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2025-05-30 22:34:40 +0530

    adding transmission 4

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -249,6 +249,7 @@
 
     environment.systemPackages = with pkgs; [
       neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      transmission_4
       python3Full
       git
       emacs
