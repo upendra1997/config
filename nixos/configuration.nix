56bfa4e0f82cccb918e25dc2d53f66ed5f232904
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2026-05-25 01:00:38 +0530

    enabling avahi

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -79,6 +79,16 @@
   services.gnome.gnome-keyring.enable = true;
 
   services.xrdp.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+    openFirewall = true;
+  };
   services.samba = {
     enable = true;
     openFirewall = true;
