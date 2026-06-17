cd44c0948bb1c98086030ee108b0f6d3d900efd9
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2026-06-17 02:16:18 +0000

    migration rpi5 to 26.05

--- nixos/rpi5/flake.nix
+++ nixos/rpi5/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Example Raspberry Pi 5 configuration flake";
     inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
       nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
     };
 
