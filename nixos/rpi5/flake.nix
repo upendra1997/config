50c6c920b61ea030c4c7d15552aab49d5d61a391
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2026-08-30 14:50:45 +0530

    auto format

--- nixos/rpi5/flake.nix
+++ nixos/rpi5/flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Example Raspberry Pi 5 configuration flake";
-    inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-      nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
+  };
 
   nixConfig = {
     extra-substituters = [
@@ -14,234 +14,258 @@
     ];
   };
 
-  outputs = { self, nixpkgs, nixos-raspberrypi }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-raspberrypi,
+    }@inputs:
     {
       nixosConfigurations = {
         pi = nixos-raspberrypi.lib.nixosSystem {
           specialArgs = inputs;
           modules = [
-            ({...}: {
-              imports = with nixos-raspberrypi.nixosModules; [
-                raspberry-pi-5.base
-                raspberry-pi-5.bluetooth
-              ];
-            })
-            ({ pkgs, lib, config, ... }:
-            let
-              normalUsers = lib.filter (u: u.isNormalUser) (lib.attrValues config.users.users);
-            in {
-              boot.loader.raspberry-pi.bootloader = "kernel";
-
-  # `d` ensures the dir exists; `L+` force-recreates a symlink, where `argument` is source
-  systemd.tmpfiles.settings."10-home-links" = lib.mkMerge (map (u: {
-    "${u.home}/.config".d = {
-      mode = "0755";
-      user = u.name;
-      group = u.group;
-    };
-    "${u.home}/.config/sway"."L+".argument = "${u.home}/config/.config/sway";
-    "${u.home}/.config/nvim"."L+".argument = "${u.home}/config/nvim";
-    "${u.home}/.wezterm.lua"."L+".argument = "${u.home}/config/.wezterm.lua";
-    "${u.home}/.tmux.conf"."L+".argument = "${u.home}/config/.tmux.conf";
-    "${u.home}/.emacs.d"."L+".argument = "${u.home}/config/.emacs.d";
-  }) normalUsers);
-
-system.stateVersion = "26.05";
-time.timeZone = "Asia/Kolkata";
-	      nix.gc = {
-	        automatic = true;
-	        randomizedDelaySec = "14m";
-	        options = "--delete-older-than 30d";
-	      };
-		
-  # Enable sound.
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.options = "eurosign:e,caps:escape";
-  services.xserver.xkb.variant = "dvorak";
-console = {
-   useXkbConfig = true;
-   earlySetup = true;
-   font = "${pkgs.terminus_font}/share/consolefonts/ter-116n.psf.gz";
-   packages = with pkgs; [ terminus_font ];
- };
-
-services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      workstation = true;
-    };
-    openFirewall = true;
-  };
-
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
-        user = "hdggxin";
-      };
-    };
-  };
-
-  programs.nix-ld.enable = true;
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
-
-  programs.fish = {
-    enable = true;
-    useBabelfish = true;
-    shellAliases = {
-      "vim" = "nvim";
-    };
-    shellInit = "set EDITOR nvim\n set VISUAL nvim";
-    vendor.completions.enable = true;
-    vendor.config.enable = true;
-    vendor.functions.enable = true;
-  };
-
-programs.direnv = {
-    enable = true;
-    silent = false;
-    loadInNixShell = true;
-    nix-direnv = {
-      enable = true;
-      package = pkgs.nix-direnv;
-    };
-  };
-
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.optimise.automatic = true;
-  nix.settings.auto-optimise-store = true;
-  nix.settings.trusted-users = ["hdggxin"];
-
-  nixpkgs.config.allowUnfree = true;
-  fonts.packages  = with pkgs; [
-iosevka
-];
-
-              environment.systemPackages = with pkgs; [
-git
-tree
-neovim
-htop
-tmux
-networkmanager
-parted
-bluetuith
-pulsemixer
-];
-              networking.hostName = "pi";
-              networking.networkmanager.enable = true;
-              networking.firewall.enable = false;
-              users.users.hdggxin = {
-                shell = pkgs.fish;
-                initialPassword = "Up1997@hdggxin";
-                isNormalUser = true;
-                extraGroups = [
-                  "wheel"
+            (
+              { ... }:
+              {
+                imports = with nixos-raspberrypi.nixosModules; [
+                  raspberry-pi-5.base
+                  raspberry-pi-5.bluetooth
                 ];
-                packages = with pkgs; [
-			firefox-esr # luakit
-			grim
-			mpv
-			swayimg
-			wezterm
-      fzf
-      emacs
-      pyright
-      clang
-      gdb
-      go
-      nodejs
-      tree-sitter
-      uv
-      rustup
-      nixd
-      nixfmt
-      gopls
-      libclang
-      clojure
-      leiningen
-      babashka
-      clojure-lsp
-      rustup
-      ghc
-		] ++
-[ # fish packages
-        fishPlugins.z
-        fishPlugins.transient-fish
-        fishPlugins.sponge
-        fishPlugins.puffer
-        fishPlugins.plugin-git
-        fishPlugins.autopair
-        fishPlugins.humantime-fish
-        fishPlugins.fzf
-        fishPlugins.done
-      ];
+              }
+            )
+            (
+              {
+                pkgs,
+                lib,
+                config,
+                ...
+              }:
+              let
+                normalUsers = lib.filter (u: u.isNormalUser) (lib.attrValues config.users.users);
+              in
+              {
+                boot.loader.raspberry-pi.bootloader = "kernel";
 
+                # `d` ensures the dir exists; `L+` force-recreates a symlink, where `argument` is source
+                systemd.tmpfiles.settings."10-home-links" = lib.mkMerge (
+                  map (u: {
+                    "${u.home}/.config".d = {
+                      mode = "0755";
+                      user = u.name;
+                      group = u.group;
+                    };
+                    "${u.home}/.config/sway"."L+".argument = "${u.home}/config/.config/sway";
+                    "${u.home}/.config/nvim"."L+".argument = "${u.home}/config/nvim";
+                    "${u.home}/.wezterm.lua"."L+".argument = "${u.home}/config/.wezterm.lua";
+                    "${u.home}/.tmux.conf"."L+".argument = "${u.home}/config/.tmux.conf";
+                    "${u.home}/.emacs.d"."L+".argument = "${u.home}/config/.emacs.d";
+                  }) normalUsers
+                );
 
-	      openssh.authorizedKeys.keys = (import ../authorized_keys.nix);
-              };
+                system.stateVersion = "26.05";
+                time.timeZone = "Asia/Kolkata";
+                nix.gc = {
+                  automatic = true;
+                  randomizedDelaySec = "14m";
+                  options = "--delete-older-than 30d";
+                };
 
-              services.openssh = {
-enable = true;
-};
-            })
+                # Enable sound.
+                security.rtkit.enable = true;
+                services.pipewire = {
+                  enable = true;
+                  alsa.enable = true;
+                  alsa.support32Bit = true;
+                  pulse.enable = true;
+                  jack.enable = true;
+                };
 
-            ({ ... }: {
+                # Configure keymap in X11
+                services.xserver.xkb.layout = "us";
+                services.xserver.xkb.options = "eurosign:e,caps:escape";
+                services.xserver.xkb.variant = "dvorak";
+                console = {
+                  useXkbConfig = true;
+                  earlySetup = true;
+                  font = "${pkgs.terminus_font}/share/consolefonts/ter-116n.psf.gz";
+                  packages = with pkgs; [ terminus_font ];
+                };
 
+                services.avahi = {
+                  enable = true;
+                  nssmdns4 = true;
+                  publish = {
+                    enable = true;
+                    addresses = true;
+                    workstation = true;
+                  };
+                  openFirewall = true;
+                };
 
-# 
-# NAME        MAJ:MIN RM  SIZE RO TYPE MOUNTPOINTS
-# sda           8:0    1 14.8G  0 disk 
-# ├─sda1        8:1    1    1G  0 part 
-# └─sda2        8:2    1 13.8G  0 part /nix/store
-#                                      /
-# mmcblk0     179:0    0 29.5G  0 disk 
-# ├─mmcblk0p1 179:1    0  512M  0 part 
-# └─mmcblk0p2 179:2    0   29G  0 part 
-# 
-# 
-# 
-# /dev/sda2: LABEL="NIXOS_SD" UUID="44444444-4444-4444-8888-888888888888" BLOCK_SIZE="4096" TYPE="ext4" PARTUUID="2175794e-02"
-# /dev/sda1: LABEL_FATBOOT="FIRMWARE" LABEL="FIRMWARE" UUID="2175-794E" BLOCK_SIZE="512" TYPE="vfat" PARTUUID="2175794e-01"
-# /dev/mmcblk0p1: LABEL_FATBOOT="FIRMWARE" LABEL="FIRMWARE" UUID="1B13-7D18" BLOCK_SIZE="512" TYPE="vfat" PARTLABEL="ESP" PARTUUID="1fadce79-8e18-4e1e-87eb-a43fce290579"
-# /dev/mmcblk0p2: LABEL="NIXOS_SD" UUID="40593f3a-13c4-4a2e-8528-3f9dee9ccac7" BLOCK_SIZE="4096" TYPE="ext4" PARTLABEL="primary" PARTUUID="166d2b5b-eea9-4f88-a0d1-218a22944311"
-# 
-              fileSystems = {
-                "/boot/firmware" = {
-                  device = "/dev/mmcblk0p1";
-                  fsType = "vfat";
-                  options = [
-                    "noatime"
-                    "noauto"
-                    "x-systemd.automount"
-                    "x-systemd.idle-timeout=1min"
+                services.greetd = {
+                  enable = true;
+                  settings = {
+                    default_session = {
+                      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
+                      user = "hdggxin";
+                    };
+                  };
+                };
+
+                programs.nix-ld.enable = true;
+                programs.sway = {
+                  enable = true;
+                  wrapperFeatures.gtk = true;
+                };
+
+                programs.fish = {
+                  enable = true;
+                  useBabelfish = true;
+                  shellAliases = {
+                    "vim" = "nvim";
+                  };
+                  shellInit = "set EDITOR nvim\n set VISUAL nvim";
+                  vendor.completions.enable = true;
+                  vendor.config.enable = true;
+                  vendor.functions.enable = true;
+                };
+
+                programs.direnv = {
+                  enable = true;
+                  silent = false;
+                  loadInNixShell = true;
+                  nix-direnv = {
+                    enable = true;
+                    package = pkgs.nix-direnv;
+                  };
+                };
+
+                nix.settings.experimental-features = [
+                  "nix-command"
+                  "flakes"
+                ];
+                nix.optimise.automatic = true;
+                nix.settings.auto-optimise-store = true;
+                nix.settings.trusted-users = [ "hdggxin" ];
+
+                nixpkgs.config.allowUnfree = true;
+                fonts.packages = with pkgs; [
+                  iosevka
+                ];
+
+                environment.systemPackages = with pkgs; [
+                  git
+                  tree
+                  neovim
+                  htop
+                  tmux
+                  networkmanager
+                  parted
+                  bluetuith
+                  pulsemixer
+                ];
+                networking.hostName = "pi";
+                networking.networkmanager.enable = true;
+                networking.firewall.enable = false;
+                users.users.hdggxin = {
+                  shell = pkgs.fish;
+                  initialPassword = "Up1997@hdggxin";
+                  isNormalUser = true;
+                  extraGroups = [
+                    "wheel"
                   ];
+                  packages =
+                    with pkgs;
+                    [
+                      firefox-esr # luakit
+                      grim
+                      mpv
+                      swayimg
+                      wezterm
+                      fzf
+                      emacs
+                      pyright
+                      clang
+                      gdb
+                      go
+                      nodejs
+                      tree-sitter
+                      uv
+                      rustup
+                      nixd
+                      nixfmt
+                      gopls
+                      libclang
+                      clojure
+                      leiningen
+                      babashka
+                      clojure-lsp
+                      rustup
+                      ghc
+                    ]
+                    ++ [
+                      # fish packages
+                      fishPlugins.z
+                      fishPlugins.transient-fish
+                      fishPlugins.sponge
+                      fishPlugins.puffer
+                      fishPlugins.plugin-git
+                      fishPlugins.autopair
+                      fishPlugins.humantime-fish
+                      fishPlugins.fzf
+                      fishPlugins.done
+                    ];
+
+                  openssh.authorizedKeys.keys = (import ../authorized_keys.nix);
                 };
-                "/" = {
-                  device = "/dev/mmcblk0p2";
-                  fsType = "ext4";
-                  options = [ "noatime" ];
+
+                services.openssh = {
+                  enable = true;
                 };
-              };
-            })
+              }
+            )
+
+            (
+              { ... }:
+              {
+
+                #
+                # NAME        MAJ:MIN RM  SIZE RO TYPE MOUNTPOINTS
+                # sda           8:0    1 14.8G  0 disk
+                # ├─sda1        8:1    1    1G  0 part
+                # └─sda2        8:2    1 13.8G  0 part /nix/store
+                #                                      /
+                # mmcblk0     179:0    0 29.5G  0 disk
+                # ├─mmcblk0p1 179:1    0  512M  0 part
+                # └─mmcblk0p2 179:2    0   29G  0 part
+                #
+                #
+                #
+                # /dev/sda2: LABEL="NIXOS_SD" UUID="44444444-4444-4444-8888-888888888888" BLOCK_SIZE="4096" TYPE="ext4" PARTUUID="2175794e-02"
+                # /dev/sda1: LABEL_FATBOOT="FIRMWARE" LABEL="FIRMWARE" UUID="2175-794E" BLOCK_SIZE="512" TYPE="vfat" PARTUUID="2175794e-01"
+                # /dev/mmcblk0p1: LABEL_FATBOOT="FIRMWARE" LABEL="FIRMWARE" UUID="1B13-7D18" BLOCK_SIZE="512" TYPE="vfat" PARTLABEL="ESP" PARTUUID="1fadce79-8e18-4e1e-87eb-a43fce290579"
+                # /dev/mmcblk0p2: LABEL="NIXOS_SD" UUID="40593f3a-13c4-4a2e-8528-3f9dee9ccac7" BLOCK_SIZE="4096" TYPE="ext4" PARTLABEL="primary" PARTUUID="166d2b5b-eea9-4f88-a0d1-218a22944311"
+                #
+                fileSystems = {
+                  "/boot/firmware" = {
+                    device = "/dev/mmcblk0p1";
+                    fsType = "vfat";
+                    options = [
+                      "noatime"
+                      "noauto"
+                      "x-systemd.automount"
+                      "x-systemd.idle-timeout=1min"
+                    ];
+                  };
+                  "/" = {
+                    device = "/dev/mmcblk0p2";
+                    fsType = "ext4";
+                    options = [ "noatime" ];
+                  };
+                };
+              }
+            )
           ];
         };
       };
